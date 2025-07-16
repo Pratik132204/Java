3f32bec2196bb190a21f47ec7cd611069b4da240
Pratik132204 <pratikpshinde2000@gmail.com>
2025-07-16 12:40:51 +0530

    java test

---
+++ Hello.java
@@ -0,0 +1,10 @@
+class Hello
+{
+      public static void main (String a[])
+      {
+        int num1 =123;
+        int num2 =234;
+        int result = num1 + num2;
+         System.out.println(result);
+      }
+    }
---
+++ Pratik.java
@@ -0,0 +1,21 @@
+
+
+
+public class  Pratik
+{
+public static void main(String[] a) 
+ {
+    int nums[]= new int  [5];
+    nums[0]=23;
+    nums[1]=24;
+    nums[2]=25;
+    nums[3]=26;
+    nums[4]=27;
+
+
+    for(int i=0;i<5;i++)
+    {
+    System.out.println(nums[i]);
+    }
+ }
+}
